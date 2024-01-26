89bf77870cd4b37be826a7d65bb82b03dd0c542b
Chris Frantz <cfrantz@google.com>
2024-01-25 16:20:58 -0800

    [rom_ext,rescue] Rescue protocol infrastructure
    1. Add function to get the mcycle timer to `ibex`.
    2. Add UART receive functions to the `uart` driver.
    3. Add `pinmux` support for UART receieve and reading strapping pins.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- sw/device/silicon_creator/lib/drivers/ibex.h
+++ sw/device/silicon_creator/lib/drivers/ibex.h
@@ -8,6 +8,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "sw/device/lib/base/csr.h"
 #include "sw/device/lib/base/macros.h"
 
 #ifdef __cplusplus
@@ -21,6 +22,18 @@ extern "C" {
  */
 OT_WARN_UNUSED_RESULT
 uint32_t ibex_fpga_version(void);
+
+#ifdef OT_PLATFORM_RV32
+OT_WARN_UNUSED_RESULT
+inline uint64_t ibex_mcycle(void) {
+  uint32_t lo, hi;
+  CSR_READ(CSR_REG_MCYCLE, &lo);
+  CSR_READ(CSR_REG_MCYCLEH, &hi);
+  return ((uint64_t)hi << 32) | lo;
+}
+#else
+extern uint64_t ibex_mcycle(void);
+#endif
 
 /**
  * The following constants represent the expected number of sec_mmio register

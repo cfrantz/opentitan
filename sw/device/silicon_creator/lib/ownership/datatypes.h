8d536943a566a637c6b167b5ff31373a0843c1c0
Chris Frantz <cfrantz@google.com>
2024-02-23 11:59:15 -0800

    [boot_svc] Add unlock and activate ownership commands
    Add boot_svc command structures for the unlock and activate ownership
    commands.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

---
+++ sw/device/silicon_creator/lib/ownership/datatypes.h
@@ -0,0 +1,29 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef LOCAL_GOOGLE_HOME_CFRANTZ_OPENTITAN_OWNER_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_DATATYPES_H_
+#define LOCAL_GOOGLE_HOME_CFRANTZ_OPENTITAN_OWNER_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_DATATYPES_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+typedef struct owner_key {
+  uint32_t key[16];
+} owner_key_t;
+
+typedef struct owner_signature {
+  uint32_t signature[16];
+} owner_signature_t;
+
+typedef struct owner_digest {
+  uint32_t digest[8];
+} owner_digest_t;
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+#endif  // LOCAL_GOOGLE_HOME_CFRANTZ_OPENTITAN_OWNER_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_DATATYPES_H_

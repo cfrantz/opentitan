9f1f8a6d729794c8822073aacf9afe418c7bcf65
Chris Frantz <cfrantz@google.com>
2024-03-18 11:56:45 -0700

    ownership command
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- sw/host/opentitantool/src/command/ownership.rs
+++ sw/host/opentitantool/src/command/ownership.rs
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::Result;
-use clap::{Args, Subcommand};
+use clap::{Args, Subcommand, ValueEnum};
 use serde_annotate::Annotate;
 use std::any::Any;
 use std::fs::{File, OpenOptions};
@@ -12,9 +12,87 @@ use std::path::PathBuf;
 use opentitanlib::app::command::CommandDispatch;
 use opentitanlib::app::TransportWrapper;
 use opentitanlib::chip::helper::OwnershipUnlockParams;
-//use opentitanlib::chip::boot_svc::{OwnershipUnlockRequest, UnlockMode};
-//use opentitanlib::crypto::ecdsa::{EcdsaPrivateKey, EcdsaPublicKey, EcdsaRawSignature};
-//use opentitanlib::util::parse_int::ParseInt;
+use opentitanlib::crypto::ecdsa::{EcdsaPrivateKey, EcdsaRawSignature};
+use opentitanlib::ownership::{OwnerBlock, TlvHeader};
+
+#[derive(ValueEnum, Debug, Clone, Copy, PartialEq)]
+enum Format {
+    Auto,
+    Text,
+    Binary,
+}
+
+#[derive(Debug, Args)]
+pub struct OwnershipConfigCommand {
+    #[arg(long, help = "Use the basic ownership block", conflicts_with = "input")]
+    basic: bool,
+    #[arg(
+        short,
+        long,
+        help = "A file containing an ownership config block",
+        conflicts_with = "basic",
+        required_unless_present("basic")
+    )]
+    input: Option<PathBuf>,
+    #[arg(long, value_enum, default_value_t = Format::Auto, help = "Input format")]
+    inform: Format,
+    #[arg(long, help = "A path to a detached signature for the unlock request")]
+    pub signature: Option<PathBuf>,
+    #[arg(long, help = "A path to a private key to sign the request")]
+    pub sign: Option<PathBuf>,
+    #[arg(help = "Binary output file path")]
+    output: Option<PathBuf>,
+}
+
+impl CommandDispatch for OwnershipConfigCommand {
+    fn run(
+        &self,
+        _context: &dyn Any,
+        _transport: &TransportWrapper,
+    ) -> Result<Option<Box<dyn Annotate>>> {
+        let mut config = if self.basic {
+            OwnerBlock::basic()
+        } else {
+            let input = std::fs::read(self.input.as_ref().unwrap())?;
+            let mut inform = self.inform;
+            if inform == Format::Auto {
+                inform = match input[0] {
+                    b'{' | b'#' | b'/' | b'\n' => Format::Text,
+                    _ => Format::Binary,
+                };
+            }
+            match inform {
+                Format::Text => {
+                    let text = std::str::from_utf8(&input)?;
+                    serde_annotate::from_str::<OwnerBlock>(text)?
+                }
+                Format::Binary => {
+                    let mut cursor = std::io::Cursor::new(&input);
+                    let header = TlvHeader::read(&mut cursor)?;
+                    OwnerBlock::read(&mut cursor, header)?
+                }
+                _ => unreachable!(),
+            }
+        };
+
+        if let Some(signature) = &self.signature {
+            let mut f = File::open(signature)?;
+            config.signature = EcdsaRawSignature::read(&mut f)?;
+        }
+        if let Some(sign) = &self.sign {
+            let key = EcdsaPrivateKey::load(sign)?;
+            config.sign(&key)?;
+        }
+
+        if let Some(output) = &self.output {
+            let mut f = OpenOptions::new().write(true).create(true).open(output)?;
+            config.write(&mut f)?;
+            Ok(None)
+        } else {
+            Ok(Some(Box::new(config)))
+        }
+    }
+}
 
 #[derive(Debug, Args)]
 pub struct OwnershipUnlockCommand {
@@ -48,5 +126,6 @@ impl CommandDispatch for OwnershipUnlockCommand {
 
 #[derive(Debug, Subcommand, CommandDispatch)]
 pub enum OwnershipCommand {
+    Config(OwnershipConfigCommand),
     Unlock(OwnershipUnlockCommand),
 }

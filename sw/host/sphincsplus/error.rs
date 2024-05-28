396bbb7f07a344d8e08e1a152587f5dd22fc7aba
Chris Frantz <cfrantz@google.com>
2024-05-28 12:49:43 -0700

    WIP: Bind multiple SPX+ algorithms into Rust
    Signed-off-by: Chris Frantz <cfrantz@google.com>

---
+++ sw/host/sphincsplus/error.rs
@@ -0,0 +1,20 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum SpxError {
+    #[error("SPHINCS+ key generation failed with error code {0}")]
+    KeyGen(i32),
+
+    #[error("SPHINCS+ signature generation failed with error code {0}")]
+    SigGen(i32),
+
+    #[error("Unexpected signature length {0}")]
+    BadSigLength(usize),
+
+    #[error("Signature did not pass verification")]
+    BadSignature,
+}
